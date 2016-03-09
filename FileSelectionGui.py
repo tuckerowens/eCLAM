d1a786774deb6342180e65c74c4aaebcc533163e
Tucker <tuckerowens@aol.com>
2016-03-08 19:39:07 -0500

    Adding Default XML config file FileRecognizer * Adding the ability to handle recognizers in xml FileSelectionGui * Modfied to use the xml version of the recognizer * Enabled changing the xml file for the recognizer

--- FileSelectionGui.py
+++ FileSelectionGui.py
@@ -1,8 +1,9 @@
 
 
 from tkinter import filedialog
+from Utils.FileRecognizer import *
 
-import sys
+import sys, os
 if sys.version_info[0] < 3:
     from Tkinter import *
 else:
@@ -22,14 +23,104 @@ class FileSelectionGui(Toplevel):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        btnSelectDir = Button(self.main, text="Select a Dataset root", command=self.directorySelected)
-        btnSelectDir.grid(sticky=N)
-        print("Built File Selection")
+        top = self.winfo_toplevel()
+        self.menuBar = Menu(top)
+        top['menu'] = self.menuBar
+
+        self.subMenu = Menu(self.menuBar)
+        self.menuBar.add_cascade(label='Configure', menu=self.subMenu)
+        self.subMenu.add_command(label='Set Config', command=self.__handleSetConfig)
+        self.configFile = "resources/defaultFileRecognizers.xml"
+
+        self.lstBoxes = {}
+        self.frameBoxes = {}
+
+        dirBox = LabelFrame(self.main, text="Selected Dataset")
+        dirBox.grid(sticky=NSEW, padx=5)
+        self.main.grid_columnconfigure(0, weight=1)
+
+        self.selectedDir = Label(dirBox, text="No directory selected")
+        self.selectedDir.grid(sticky=NSEW)
+        btnSelectDir = Button(dirBox, text="Browse", command=self.directorySelected)
+        btnSelectDir.grid(row=0, column=1, sticky=E)
+        dirBox.grid_columnconfigure(0, weight=5)
+        dirBox.grid_columnconfigure(1, weight=1)
+
+        fileTypesFrame = LabelFrame(self.main, text="Matching File Types")
+        fileTypesFrame.grid(sticky=NSEW, padx=5)
+        self.lstFileTypes = Listbox(fileTypesFrame, exportselection=0)
+        self.lstFileTypes.bind("<<ListboxSelect>>", self.fileTypeSelected)
+        self.lstFileTypes.grid(sticky=NSEW, padx=2)
+        fileTypesFrame.grid_rowconfigure(0, weight=1)
+        fileTypesFrame.grid_columnconfigure(0, weight=1)
+
+        self.componentFrame = LabelFrame(self.main, text="File Components")
+        self.componentFrame.grid(sticky=NSEW)
+        fileTypesFrame.grid(sticky=NSEW, padx=5)
+        self.main.grid_rowconfigure(1, weight=0)
+        self.main.grid_rowconfigure(2, weight=5)
+
+        btnOkay = Button(self.main, text="Okay", command=self.submit)
+        btnOkay.grid(sticky=E)
+
+
+    def __handleSetConfig(self):
+        selected = filedialog.askopenfilename(filetypes=[("XML File", ".xml")])
+        if selected == "":
+            return
+        self.configFile = selected
+
+
+
+    def submit(self):
+        print ("Create dataset")
 
 
     def directorySelected(self):
         selected = filedialog.askdirectory()
         if selected == "":
             return
-        self.handler.handleFileSelectionResponce(selected)
+        self.selectedDir.configure(text=selected)
+        self.fileTypes = {}
+        for dirpath, dirname, filenames in os.walk(selected):
+            temp =  XmlFileRecognizer.recognizeBatch([f for f in filenames],self.configFile)
+            for key in temp.keys():
+                if key in self.fileTypes.keys():
+                    self.fileTypes[key] = self.fileTypes[key]+temp[key]
+                else:
+                    self.fileTypes[key] = temp[key]
 
+        for f in self.fileTypes.keys():
+            self.lstFileTypes.insert(END, str(f))
+
+    def fileTypeSelected(self, index):
+        cls = list(self.fileTypes.keys())[self.lstFileTypes.curselection()[0]]
+        print(cls)
+        elements = {}
+        for f in self.fileTypes[cls]:
+            temp = cls.componentExtraction(f)
+            for k in temp.keys():
+                if k in elements.keys():
+                    elements[k].add(temp[k])
+                else:
+                    elements[k] = set()
+                    elements[k].add(temp[k])
+        for k in elements.keys():
+            if k in self.lstBoxes.keys() or k == 'fileno':
+                print(k + " already built")
+            else:
+                self.frameBoxes[k] = LabelFrame(self.componentFrame, text=k)
+                self.frameBoxes[k].grid(row=int(2+((len(self.frameBoxes.keys())-1)/3)), column=(len(self.frameBoxes.keys())-1)%3, sticky=NSEW)
+                self.lstBoxes[k] = Listbox(self.frameBoxes[k], exportselection=0)
+                self.lstBoxes[k].bind("<<ListboxSelect>>", self.optionSelected)
+                self.lstBoxes[k].grid(sticky=NSEW)
+                self.frameBoxes[k].grid_columnconfigure(0, weight=1)
+                self.frameBoxes[k].grid_rowconfigure(0, weight=1)
+                for item in elements[k]:
+                    self.lstBoxes[k].insert(END, str(item))
+                if len(elements[k]):
+                    self.lstBoxes[k].selection_set(0)
+
+    def optionSelected(self, event):
+        for k in self.lstBoxes.keys():
+            print(k + str(self.lstBoxes[k]))
--- Utils/FileRecognizer.py
+++ Utils/FileRecognizer.py
@@ -1,22 +1,105 @@
 
-import re
-from dateutil.parser import parse
+import re, xml.etree.ElementTree as ET
+
 
 class FileRecognizer:
 
-    @staticmethod
-    def validFile(name, elements):
+    # This function should be overridden
+    def validFile(self, name, elements):
         return False
 
-    @staticmethod
-    def componentExtraction(name):
+    # This function should be overridden to
+    # extract all the information elements from a file name
+    def componentExtraction(self, name):
         return {}
+
+    # This needs to be updated when new
+    # file recognizers are written
+    @staticmethod
+    def recognize(filename):
+        if FileTypeCV2.validFile(filename):
+            return FileTypeCV2
+        return None
+
+#This object will take a config file and generate
+class XmlFileRecognizer(FileRecognizer):
+
+    configFile = ""
+    recognizers = []
+
+    def __init__(self, element):
+        self.name = element.get("name")
+        try:
+            self.matchingPattern = re.compile(element.find("RegexMatcher").text)
+        except:
+            print("Regex rule to match file name is invalid for recognizer %s" % (self.name))
+            self.matchingPattern = re.compile("")
+        elementList = element.find("ExtractableElements")
+        try:
+            self.elementPattern = re.compile(elementList.find("RegexExpression").text)
+        except:
+            self.elementPattern = re.compile("")
+            self.elementGroups = {}
+            print("Regex rule to match file name components is invalid for recognizer %s" % (self.name))
+            return
+        self.elementGroups = {}
+        for eg in elementList.iter("Element"):
+            self.elementGroups[eg.get("name")] = eg.get("group")
+
+    def __str__(self, *args, **kwargs):
+        return self.name
+
+    def validFile(self, name, elements):
+        if not self.matchingPattern.match(name): return False;
+        selfElems = self.componentExtraction(name);
+        for k in selfElems.keys():
+            if k in elements.keys():
+                if not selfElems[k] in elements[k]:
+                    return False
+        return True
+
+    def componentExtraction(self, name):
+        output = {}
+        result = self.elementPattern.search(name)
+        if result == None:
+            return output
+        for k in self.elementGroups.keys():
+            output[k] = result.group(int(self.elementGroups[k]))
+        return  output
+
+
+    @staticmethod
+    def buildRecognizers():
+        tree = ET.parse(XmlFileRecognizer.configFile)
+        root = tree.getroot()
+        XmlFileRecognizer.recognizers = []
+        for fileRecognizer in root.iter("FileRecognizer"):
+            XmlFileRecognizer.recognizers.append(XmlFileRecognizer(fileRecognizer))
+
+    @staticmethod
+    def findRecognizerByName(name):
+        options = list(filter(lambda x: str(x) == name, XmlFileRecognizer.recognizers))
+        if len(options) > 1:
+            print("I'm confused, multiple recognizers have the name " + name)
+        if len(options) == 0: return None
+        return options[0]
+
+    @staticmethod
+    def recognizeBatch(files, definitionFile):
+        if definitionFile != XmlFileRecognizer.configFile:
+            XmlFileRecognizer.configFile = definitionFile
+            XmlFileRecognizer.buildRecognizers()
+        output = {}
+        for recognizer in XmlFileRecognizer.recognizers:
+            output[recognizer] = list(filter(lambda x: recognizer.validFile(x, {}), files))
+        return output
+
+
 
 
 class FileTypeCV2(FileRecognizer):
 
-    @staticmethod
-    def validFile(name, elements):
+    def validFile(name, elements=None):
         if elements == {} or elements == None:
             return "CV2" in name or "DTA" in name
         elems = FileTypeCV2.componentExtraction(name)
@@ -29,15 +112,14 @@ class FileTypeCV2(FileRecognizer):
         return True
 
 
-    @staticmethod
     def componentExtraction(name):
         elems = re.search("([0-9]+)\s([A-Z]+)\s([1-9][0-9]+[n|u|m]M)\s([1-9]+)\s([A-F]+)\s([0-9]+)\s+CV2_#([0-9]+).DTA", name)
         output = {}
-        output["Date"] = parse(elems.group(0))
-        output["Chem"] = elems.group(1)
-        output["Concentration"] = elems.group(2)
-        output["trial"] = int(elems.group((3)))
-        output["scientist"] = elems.group(4)
-        output["idk"] = elems.group(5)
-        output["fileno"] = int(elems.group(6))
+        output["Date"] = elems.group(1)
+        output["Chem"] = elems.group(2)
+        output["Concentration"] = elems.group(3)
+        output["trial"] = elems.group((4))
+        output["scientist"] = elems.group(5)
+        output["idk"] = elems.group(6)
+        output["fileno"] = int(elems.group(7))
         return output
