3d9a0ae4765b20129bc2416f9e97d69306868631
Tucker <tuckerowens@aol.com>
2016-04-06 10:33:51 -0400

    Fix the min max output, also speed it up

--- Dataset.py
+++ Dataset.py
@@ -3,6 +3,7 @@
 ## Dataset
 ######################################################################
 import time
+import numpy as np
 class Dataset:
     """
     Dataset is an interface that provides several methods that subclasses need to implement.
@@ -88,41 +89,34 @@ class Dataset:
     def getMaxCoord(self):
         if self.gMaxCoord == None:
             m = self.getMax()
-            self.gMaxCoord = self.getCoordsOfValue(m)
+            self.gMaxCoord = self.getCoordsOfValue(m)[0]
         return self.gMaxCoord
 
 
     def getCoordsOfValue(self, m):
         start = time.time()
         plane = self.getPlane()
-        cols = [x for x in range(len(self.getHorizontalAt(0))) if x in plane[x]]
-        print("\tcols : %fs" % (time.time() - start))
+        cols = [x for x in range(len(self.getHorizontalAt(0))) if m in plane[x]]
         rows = [x for sublist in [list(filter(lambda x: l[x] == m, range(len(l)))) for l in self.getPlane()] for x in sublist]
-        print("\trows : %fs" % (time.time() - start))
         matches = zip(cols, rows)
-        self.gMaxCoord = list(matches)
-        print("Get %f Coord Gen : %fs" % (m, time.time() - start))
+        return (list(matches))
 
     def getMinCoord(self):
         if self.gMinCoord == None:
-            m = self.getMax()
-            self.gMinCoord = self.getCoordsOfValue(m)
+            m = self.getMin()
+            self.gMinCoord = self.getCoordsOfValue(m)[0]
         return self.gMinCoord
 
 
     def getMin(self):
         if self.min == None:
-            start = time.time()
             self.min =  min([min(i) for i in self.getPlane()])
-            print("Found Min in %f" % (time.time() - start))
 
         return self.min
 
     def getMax(self):
         if self.max == None:
-            start = time.time()
-            self.max = max([max(i) for i in self.getPlane()])
-            print("Found Max in %f" % (time.time() - start))
+            self.max = max(np.array(self.getPlane()).flatten())
         return self.max
 
 
@@ -136,6 +130,6 @@ class Dataset:
         """
 
         outstr = str(self) + '\n'
-        outstr += "  Min of %f @ %s" % (self.getMin(), str(self.getMinCoord())) + '\n'
-        outstr += "  Max of %f @ %s" % (self.getMax(), str(self.getMaxCoord()))
+        outstr += "  Min %s @ %s" % (str(self.getMin()), str(self.getMinCoord())) + '\n'
+        outstr += "  Max %s @ %s" % (str(self.getMax()), str(self.getMaxCoord()))
         return outstr
